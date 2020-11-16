77642b47d1adfd37257171f32ea3522d886548d6
Gant Laborde <gantman@gmail.com>
2020-11-16 14:40:18 -0600

    finished trained model

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as tf from "@tensorflow/tfjs";
 import "./styles.css";
 
 const inputShape = [12, 12, 1];
+const epochs = 10
 const diceData = require("./dice_data.json").dice;
 
 // Wrap in a tidy for memory
@@ -41,8 +42,38 @@ const [stackedX, stackedY] = tf.tidy(() => {
   return [xs, ys];
 });
 
+const testModel = (model, img2Test) => {
+  const img = new Image()
+  img.crossOrigin = "anonymous";
+  img.src = img2Test;
+  img.onload = async () => {
+    const imgTensor = tf.browser.fromPixels(img, 1).expandDims();
+
+    const tensorResults = model.predict(imgTensor)
+    const results = tensorResults.arraySync()
+
+    console.log(`${img2Test} returned ${results}`)
+
+  };
+}
+
 const doLinearPrediction = async () => {
   const model = tf.sequential();
+
+  // model.add(tf.layers.conv2d({
+  //   inputShape: inputShape,
+  //   kernelSize: 5,
+  //   filters: 8,
+  //   strides: 1,
+  //   activation: 'relu',
+  //   kernelInitializer: 'varianceScaling'
+  // }));
+
+  // model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+  model.add(tf.layers.flatten(
+    {inputShape}
+  ))
 
   model.add(
     tf.layers.dense({
@@ -77,19 +108,27 @@ const doLinearPrediction = async () => {
   // Make loss callback
   const printCallback = {
     onEpochEnd: (epoch, log) => {
-      console.log(log);
+      console.log(`${epoch+1} of ${epochs}`, log);
     },
   };
 
-  console.log("starting fit");
   await model.fit(stackedX, stackedY, {
-    epochs: 100,
+    epochs,
     shuffle: true,
     batchSize: 32,
     callbacks: printCallback,
   });
 
-  // DO A TEST HERE????????
+  // Basic test
+  testModel(model, "/1.png")
+  testModel(model, "/2.png")
+  testModel(model, "/3.png")
+  testModel(model, "/4.png")
+  testModel(model, "/5.png")
+  testModel(model, "/6.png")
+  testModel(model, "/7.png")
+  testModel(model, "/8.png")
+  testModel(model, "/9.png")
 
   // for save button
   window.model = model;
@@ -105,20 +144,19 @@ class App extends React.Component {
     doLinearPrediction().then((result) =>
       this.setState({ simplePredict: result })
     );
-    console.log(stackedX.shape)
-    console.log(stackedY.shape)
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Dice Trainer 🎲</h1>
+        <h1>Dice Trainer <span role="img" aria-label="die">🎲</span></h1>
         <img
           src="/ess70.png"
           width="150"
+          alt="Gant cartoon"
         />
         <h3>
-          <a href="http://gantlaborde.com/">By Gant Laborde</a>
+          <a href="http://gantlaborde.com/" >By Gant Laborde</a>
         </h3>
         <hr/>
         <h2>{this.state.simplePredict}</h2>
